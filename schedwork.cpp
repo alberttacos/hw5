b0cc131e89079ce6e8b5b81bc5e64a0a256bce31
Albert Chung <chungalb@usc.edu>
2023-04-06 23:34:19 +0000

    removed tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -24,6 +24,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add your implementation of schedule() and other helper functions here
+bool schedulechecker(const AvailabilityMatrix& avail,
+const size_t dailyNeed,
+const size_t maxShifts,
+DailySchedule& sched,
+int days,
+int workers,
+vector<int> workeddays
+);
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +45,60 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    int days = avail.size();
+    int workers = avail[0].size();
 
-
-
-
+    vector<int> workeddays;
+    for(int i = 0; i < workers; i++){
+        workeddays.push_back(0);
+    }
+    return schedulechecker(avail, dailyNeed, maxShifts, sched, days, workers, workeddays);
 }
 
+bool schedulechecker(const AvailabilityMatrix& avail,
+const size_t dailyNeed,
+const size_t maxShifts,
+DailySchedule& sched,
+int days,
+int workers,
+vector<int> workeddays
+){
+    if((int)sched.size() == days){
+        if(sched[days - 1].size() == dailyNeed){
+            return true;
+        }
+    }
+    if(sched.size() == 0){
+        vector<Worker_T> temp; 
+        sched.push_back(temp);
+    }
+
+    int day = sched.size();
+
+    if((int) sched[day - 1].size() == (int)dailyNeed){
+        vector<Worker_T> temp;
+        sched.push_back(temp);
+    }
+    day = sched.size();
+
+    for(int i = 0; i < workers; i++){
+        if(!(find(sched[day - 1].begin(), sched[day - 1].end(), i) != sched[day - 1].end())){
+            if(avail[day - 1][i]){
+                sched[day - 1].push_back(i);
+                if(workeddays[i] < (int) maxShifts){
+                    workeddays[i]++;
+                
+                if(schedulechecker(avail, dailyNeed, maxShifts, sched, days, workers,workeddays)){
+                    return true;
+                }
+                workeddays[i]--;
+            }
+            sched[day - 1].pop_back();
+        }
+    }
+}
+if(sched[day - 1].size() == 0){
+    sched.pop_back();
+}
+return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -9,11 +9,12 @@
 
 #include "wordle.h"
 #include "dict-eng.h"
+#include <string>
 using namespace std;
 
 
 // Add prototypes of helper functions here
-
+set<string> findstring(string in, string floating, int n, int count);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,53 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    int counter = 0;
 
+    for(int i = 0; i < (int)in.length(); i++){
+        if(in[i] == '-'){
+            counter++;
+        }
+    }
+    set<string> tempset = findstring(in,floating, 0, counter);
+    set<string> finalset;
+
+
+    for(set<string>::iterator it = tempset.begin(); it != tempset.end(); ++it){
+        if(dict.find(*it) != dict.end()){
+            finalset.insert(*it);
+        }
+    }
+    return finalset;
 }
 
+set<string> findstring(string in, string floating, int n, int count){
+    if(n == (int)in.length()){
+        set<string> temp = {in};
+        return temp;
+    }
+    set<string> finalanswer;
+    if(in[n] == '-'){
+        for(int i = 0; i < (int) floating.length(); i++){
+            in[n] = floating[i];
+            string tempfloat = floating.substr(0,i) + floating.substr(i+1);
+            set <string> temp = findstring(in, tempfloat, n + 1, count - 1);
+            finalanswer.insert(temp.begin(), temp.end());
+
+            in[n] = '-';
+        }
+        if((int)floating.length() < count){
+            for(int i = 0; i < 26; i++){
+                in[n] = (char)(i + 97);
+                set<string> temp = findstring(in, floating, n + 1, count - 1);
+                finalanswer.insert(temp.begin(), temp.end());
+
+                in[n] ='-';
+            }
+        }
+    }else {
+        set<string>temp = findstring(in,floating,n+1, count);
+        finalanswer.insert(temp.begin(), temp.end());
+    }
+    return finalanswer;
+}
 // Define any helper functions here
